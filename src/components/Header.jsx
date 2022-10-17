12ab360d8c6879ab6f41a9dd19b1b0bdc73ed9af
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-17 14:28:21 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,12 +39,12 @@ function Header() {
                 <div className="header-search">
                   <form>
                     <select className="input-select">
-                      <option value="0">All Categories</option>
-                      <option value="1">Category 01</option>
-                      <option value="1">Category 02</option>
+                      <option value="0">Kategoriler</option>
+                      <option value="1">Kategori 01</option>
+                      <option value="1">Kategori 02</option>
                     </select>
                     <input className="input" placeholder="Search here" />
-                    <button className="search-btn">Search</button>
+                    <button className="search-btn">Arama</button>
                   </form>
                 </div>
               </div>
@@ -136,7 +136,7 @@ function Header() {
           <div id="responsive-nav">
             <ul className="main-nav nav navbar-nav">
               <li className="active">
-                <a href="!#">Home</a>
+                <a href="!#">AnaSayfa</a>
               </li>
               <li>
                 <a href="!#">Erkek</a>
